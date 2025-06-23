4724446f9be819b09eb2a317cffe1d99b3ecd049
Fabien Bellanger <fabien.bellanger@apitic.com>
2025-06-23 15:43:51 +0200

    Fix invalid traceId

--- src/server/axum/response.rs
+++ src/server/axum/response.rs
@@ -1,8 +1,9 @@
 //! API response module
 
+use axum::Json;
 use axum::http::StatusCode;
 use axum::response::{IntoResponse, Response};
-use axum::Json;
+use opentelemetry::TraceId;
 use opentelemetry::trace::TraceContextExt;
 use serde::Serialize;
 use thiserror::Error;
@@ -93,64 +94,51 @@ impl ApiError {
     fn response(code: StatusCode, message: &str) -> impl IntoResponse + '_ {
         let ctx = tracing::Span::current().context();
         let trace_id = ctx.span().span_context().trace_id();
-        dbg!(&trace_id);
-        let trace_id = trace_id.to_string();
+        let trace_id = if trace_id == TraceId::INVALID {
+            None
+        } else {
+            Some(trace_id.to_string())
+        };
 
         match code {
             StatusCode::REQUEST_TIMEOUT => (
                 StatusCode::REQUEST_TIMEOUT,
-                Json(ApiErrorResponse::new(
-                    StatusCode::REQUEST_TIMEOUT,
-                    message,
-                    Some(trace_id),
-                )),
+                Json(ApiErrorResponse::new(StatusCode::REQUEST_TIMEOUT, message, trace_id)),
             ),
             StatusCode::TOO_MANY_REQUESTS => (
                 StatusCode::TOO_MANY_REQUESTS,
-                Json(ApiErrorResponse::new(
-                    StatusCode::TOO_MANY_REQUESTS,
-                    message,
-                    Some(trace_id),
-                )),
+                Json(ApiErrorResponse::new(StatusCode::TOO_MANY_REQUESTS, message, trace_id)),
             ),
             StatusCode::METHOD_NOT_ALLOWED => (
                 StatusCode::METHOD_NOT_ALLOWED,
-                Json(ApiErrorResponse::new(
-                    StatusCode::METHOD_NOT_ALLOWED,
-                    message,
-                    Some(trace_id),
-                )),
+                Json(ApiErrorResponse::new(StatusCode::METHOD_NOT_ALLOWED, message, trace_id)),
             ),
             StatusCode::PAYLOAD_TOO_LARGE => (
                 StatusCode::PAYLOAD_TOO_LARGE,
-                Json(ApiErrorResponse::new(
-                    StatusCode::PAYLOAD_TOO_LARGE,
-                    message,
-                    Some(trace_id),
-                )),
+                Json(ApiErrorResponse::new(StatusCode::PAYLOAD_TOO_LARGE, message, trace_id)),
             ),
             StatusCode::BAD_REQUEST => (
                 StatusCode::BAD_REQUEST,
-                Json(ApiErrorResponse::new(StatusCode::BAD_REQUEST, message, Some(trace_id))),
+                Json(ApiErrorResponse::new(StatusCode::BAD_REQUEST, message, trace_id)),
             ),
             StatusCode::UNAUTHORIZED => (
                 StatusCode::UNAUTHORIZED,
-                Json(ApiErrorResponse::new(StatusCode::UNAUTHORIZED, message, Some(trace_id))),
+                Json(ApiErrorResponse::new(StatusCode::UNAUTHORIZED, message, trace_id)),
             ),
             StatusCode::FORBIDDEN => (
                 StatusCode::FORBIDDEN,
-                Json(ApiErrorResponse::new(StatusCode::FORBIDDEN, message, Some(trace_id))),
+                Json(ApiErrorResponse::new(StatusCode::FORBIDDEN, message, trace_id)),
             ),
             StatusCode::NOT_FOUND => (
                 StatusCode::NOT_FOUND,
-                Json(ApiErrorResponse::new(StatusCode::NOT_FOUND, message, Some(trace_id))),
+                Json(ApiErrorResponse::new(StatusCode::NOT_FOUND, message, trace_id)),
             ),
             StatusCode::SERVICE_UNAVAILABLE => (
                 StatusCode::SERVICE_UNAVAILABLE,
                 Json(ApiErrorResponse::new(
                     StatusCode::SERVICE_UNAVAILABLE,
                     message,
-                    Some(trace_id),
+                    trace_id,
                 )),
             ),
             StatusCode::UNPROCESSABLE_ENTITY => (
@@ -158,7 +146,7 @@ impl ApiError {
                 Json(ApiErrorResponse::new(
                     StatusCode::UNPROCESSABLE_ENTITY,
                     message,
-                    Some(trace_id),
+                    trace_id,
                 )),
             ),
             _ => (
@@ -166,7 +154,7 @@ impl ApiError {
                 Json(ApiErrorResponse::new(
                     StatusCode::INTERNAL_SERVER_ERROR,
                     message,
-                    Some(trace_id),
+                    trace_id,
                 )),
             ),
         }
